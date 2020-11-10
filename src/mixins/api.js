84bb59c997467ece78b83b9d603d707f7f154fa9
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2020-11-09 19:14:15 -0500

    env

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -4,13 +4,30 @@ export default {
   },
   methods: {
     url () {
-       if (window.location.host.includes('localhost')) {
+      if (window.location.host.includes('localhost')) {
         return process.env.VUE_APP_URL_API
       } if (window.location.host.includes('production')) {
         return process.env.VUE_APP_URL_API_PRODUCTION
       } else {
         return process.env.VUE_APP_URL_API_PRODUCTION
       }
+    },
+    headers () {
+      var data = JSON.parse(localStorage.getItem('token'))
+      console.log(data['access-token'])
+      const access = data['access-token']
+      const token_typ = data['access-token']
+      const clien = data['client']
+      const exp = data['expiry']
+      const ui = data['uid']
+      let headers = {
+        'access-token': access,
+        'token-type': token_typ,
+        'client': clien,
+        'expiry': exp,
+        'uid': ui
+      }
+      return headers
     },
 
     headersWithouthToken () {
@@ -24,7 +41,7 @@ export default {
       let res = axios({
         method: 'POST',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/' + path,
         headers: this.headers()
       })
       return res
@@ -33,7 +50,7 @@ export default {
       let res = axios({
         method: 'GET',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/'+ path,
         headers: this.headers()
       })
       return res
@@ -42,7 +59,7 @@ export default {
       let res = axios({
         method: 'PUT',
         data: data,
-        url: this.url() + path,
+        url:'https://control-expense.herokuapp.com/' + path,
         headers: this.headers()
       })
       return res
@@ -51,7 +68,7 @@ export default {
       let res = axios({
         method: 'DELETE',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/'+ path,
         headers: this.headers()
       })
       return res
@@ -60,7 +77,7 @@ export default {
       let res = axios({
         method: 'POST',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/' + path,
         headers: this.headersWithouthToken()
       })
       return res
@@ -69,7 +86,7 @@ export default {
       let res = axios({
         method: 'GET',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/'+ path,
         headers: this.headersWithouthToken()
       })
       return res
@@ -78,7 +95,7 @@ export default {
       let res = axios({
         method: 'PUT',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/' + path,
         headers: this.headersWithouthToken()
       })
       return res
@@ -87,7 +104,7 @@ export default {
       let res = axios({
         method: 'PUT',
         data: data,
-        url: this.url() + path,
+        url: 'https://control-expense.herokuapp.com/'+ path,
         headers: this.headersWithouthToken()
       })
       return res
